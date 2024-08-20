fad47b3e985284c209524eab3c2ee0a118fb5090
Nahuel Bocanegra <nahuboca7@gmail.com>
2024-08-20 17:57:01 -0300

    segundo commit

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -1,20 +1,27 @@
+import { useState } from "react";
 
 
 function Email (params) {
+    
 
+    const [correo,setCorreo]=useState("")
+    const [asunto,setAsunto]=useState("")
+    const [mensaje,setMensaje]=useState("")
+
+  
     return(
-        <section>
-            <form action="">
-                <div>
+        <section className="cont-email">
+            <form action="Post" className="form-email">
+                <div className="cont-correo">
                     <label  htmlFor="correo">Correo</label>
                     <input id="correo" type="text" placeholder="Correo"/>
                 </div>
-                <div>
+                <div className="cont-asunto">
                     <label htmlFor="asunto">Asunto</label>
                     <input id="asunto" type="text" placeholder="Asunto"/>
                 </div>
-                <textarea name="" id="" placeholder="Menjases"></textarea>
-
+                <textarea  className="text-mensaje"name="" id="" placeholder="Menjases"></textarea>
+                <button className="btn-enviar"><i className='bx bxs-send'></i></button>
             </form>
         </section>
     )
