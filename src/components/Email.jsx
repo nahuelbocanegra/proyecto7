57e411222497d68bcf5fdac9f7dbcd4c6f575488
Nahuel Bocanegra <nahuboca7@gmail.com>
2024-08-21 13:07:40 -0300

    tercer commit

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -2,28 +2,53 @@ import { useState } from "react";
 
 
 function Email (params) {
+
+    const [sent,setSent]=useState(false)
     
 
     const [correo,setCorreo]=useState("")
     const [asunto,setAsunto]=useState("")
     const [mensaje,setMensaje]=useState("")
 
+    const time=()=>{
+
+        setTimeout(()=>{
+            setSent(false)        
+        },3000)
+    }
+
+
+    
+    const initialcontent=()=>{
+        setAsunto("")
+        setCorreo("")
+        setMensaje("")
+        setSent(true)
+        time()
+    }
+
   
     return(
+        <>
         <section className="cont-email">
-            <form action="Post" className="form-email">
+            {sent? <div className="check"><i className='bx bx-check'></i></div> : 
+            
+
+            <form action="" className="form-email">
                 <div className="cont-correo">
                     <label  htmlFor="correo">Correo</label>
-                    <input id="correo" type="text" placeholder="Correo"/>
+                    <input value={correo} onChange={e => setCorreo(e.target.value)} id="correo" type="email" />
                 </div>
                 <div className="cont-asunto">
                     <label htmlFor="asunto">Asunto</label>
-                    <input id="asunto" type="text" placeholder="Asunto"/>
+                    <input value={asunto} onChange={e => setAsunto(e.target.value)} id="asunto" type="text" />
                 </div>
-                <textarea  className="text-mensaje"name="" id="" placeholder="Menjases"></textarea>
-                <button className="btn-enviar"><i className='bx bxs-send'></i></button>
+                <textarea  value={mensaje} onChange={e => setMensaje(e.target.value)} className="text-mensaje"name="" id="" placeholder="Menjases"></textarea>
+                <button className="btn-enviar" onClick={initialcontent}><i className='bx bxs-send'></i></button>
             </form>
+        }
         </section>
+        </>
     )
 }
 export default Email;
